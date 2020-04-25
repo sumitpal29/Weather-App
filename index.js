0d70db6735c8ec5f3c87985f5c6fc4b287bffe1a
sumitpal29 <sumit@fusioncharts.com>
2020-04-26 03:45:26 +0530

    async weather app

--- index.js
+++ index.js
@@ -1,35 +1,43 @@
-var http = require('http');
-var fs = require('fs');
-var request = require('request');
-var index = fs.readFileSync('index.html');
+const request = require("postman-request");
+const url = (name) =>
+  `http://api.weatherstack.com/current?access_key=a79f13a481e267a05222fbedb2eaae94&query=${name}`;
+const geoLoc =
+  "https://api.mapbox.com/geocoding/v5/mapbox.places/bangalore.json?access_token=pk.eyJ1Ijoic3VtaXRwYWwyOSIsImEiOiJjazlnM3J1MjUwanRzM2tudWYxdDB5NG0xIn0.Jk0_pXoJ9C4hqwm5yOlNPw";
 
-// var server = http.createServer(function(req,res){
-//     res.writeHead(200, {'Content-Type': 'text/html'});
-//     res.end(index);
-// }).listen(8080);
-//https://github.com/nshntarora/Indian-Cities-JSON/blob/master/cities.json
+const localData = {
+  geoLocations: {},
+};
 
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
-app.use('/', express.static(__dirname + '/'));
-app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+const getWeatherData = (name, cb) => {
+  if (name && cb) {
+    request({ url: url(name), json: true }, cb);
+  } else {
+    console.error("Provide a name of a place!!");
+  }
+};
 
-// POST http://localhost:8080/api
-// parameters sent with 
-app.post('/api/', function(req, res) {
-    var cap = req.body.id;
-    var url = "http://api.openweathermap.org/data/2.5/weather?q="+cap+",IN&appid=b458fbe5be41e08602b621a08fad02a7&units=metric"
-    var data;
-    request(url, function(err, response, body){
-        if(response.statusCode === 200){
-            data = JSON.parse(body);
-            res.end(data.main.temp+'');
-        }
-    })
-    
+getWeatherData("bangalore", (err, response) => {
+  if (err) {
+    console.error(`Error occured`, err);
+  }
+  const { body } = response;
+  if (body.error) {
+    console.error(`Error occured`, body.error);
+  } else {
+    const currentData = body.current;
+    console.log(currentData.temperature);
+  }
 });
 
-
-app.listen(3000, function() { console.log('listening'); });
+// request({ url: geoLoc, json: true }, function (err, response) {
+//   if (err) {
+//     console.error(`Error occured`, err);
+//   }
+//   const { body } = response;
+//   const features = body.features;
+//   if (features.length) {
+//     console.log(`Coordinate ${features[0].center.join(" ")}`);
+//   } else {
+//     console.log("No results found for the search keywork!!");
+//   }
+// });
