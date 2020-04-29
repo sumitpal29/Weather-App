b93ae38815ff306b86a02bb43c32cb9ac04a5c1b
Sumit Pal <sumitwuthyou@gmail.com>
2020-04-30 03:06:52 +0530

    basic template done for weather app

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const path = require("path"); // core-module cross os path manipulation
 const express = require("express");
+const hbs = require('hbs');
+
 const getWeatherData = require("./src/get-weater");
 const getLocationData = require("./src/get-location");
+// Application
 const app = express();
+//directories
+const publicDirectory = path.join(__dirname, "./public");
+const views = path.join(__dirname, "./templates/views");
+const partialPath = path.join(__dirname, "./templates/partials");
 
 // serving a static dicrectory
 // order app to use express.static(dir)
-app.use(express.static(path.join(__dirname, "./public")));
+app.use(express.static(publicDirectory));
+// set up view engine with express -> key - value
+app.set("view engine", "hbs");
+// setting up custom directory for views
+app.set('views', views);
+// Setting up partials
+hbs.registerPartials(partialPath)
 
 app.get("", (req, res) => {
-  let weatherStr = "";
+  // let weatherStr = "";
   if (req.query && req.query.location) {
     getLocationData(
       req.query.location,
@@ -21,23 +34,30 @@ app.get("", (req, res) => {
             if (error) {
               console.log(error);
             } else {
-              weatherStr = `weather forcast for ${place} - Temprature ${weatherData} degree celcius.`;
-              res.send(weatherStr || "Hello Express Server!!");
+              // weatherStr = `weather forcast for ${place} - Temprature ${weatherData} degree celcius.`;
+              // res.send(weatherStr || "Hello Express Server!!");
+              res.render("index", {
+                name: "Sumit",
+                place,
+                weatherData,
+              });
             }
           });
         }
       }
     );
   } else {
-    res.send(`<h1>Express</h1>`);
+    res.render("index", {
+      name: "sumit",
+    });
   }
 });
 
 app.get("/weather", (req, res) => {
   res.status(200).send({
     temprature: 20,
-    metric: "celcius"
-  })
+    metric: "celcius",
+  });
 });
 
 app.listen("3000", () => {
